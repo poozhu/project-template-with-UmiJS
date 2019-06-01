ab1ce0abfd64262cfaabd53fbe3aafb9e99448c6
zy <zy2071@126.com>
2019-06-02 00:14:35 +0800

    Update .umirc.js

--- .umirc.js
+++ .umirc.js
@@ -1,9 +1,7 @@
 
-// ref: https://umijs.org/config/
 export default {
   treeShaking: true,
   plugins: [
-    // ref: https://umijs.org/plugin/umi-plugin-react.html
     ['umi-plugin-react', {
       antd: false,
       dva: {
